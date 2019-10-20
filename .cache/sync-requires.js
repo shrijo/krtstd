3a8cb1080c0114bc58db90c405f1e31f4047c76e
shrijo <jonas.shriqui@gmail.com>
2019-10-20 12:51:04 +0200

    after develop

--- .cache/sync-requires.js
+++ .cache/sync-requires.js
@@ -5,7 +5,9 @@ const preferDefault = m => m && m.default || m
 
 
 exports.components = {
+  "component---cache-dev-404-page-js": hot(preferDefault(require("/Users/shrijo/github/krtstd/.cache/dev-404-page.js"))),
   "component---src-pages-404-js": hot(preferDefault(require("/Users/shrijo/github/krtstd/src/pages/404.js"))),
-  "component---src-pages-index-js": hot(preferDefault(require("/Users/shrijo/github/krtstd/src/pages/index.js")))
+  "component---src-pages-index-js": hot(preferDefault(require("/Users/shrijo/github/krtstd/src/pages/index.js"))),
+  "component---src-pages-page-2-js": hot(preferDefault(require("/Users/shrijo/github/krtstd/src/pages/page-2.js")))
 }
 
