2116f9ec56cfcab262a7d85b4bd832f31abe5479
shrijo <jonas.shriqui@gmail.com>
2019-10-21 10:30:49 +0200

    anothertry componentdidmount

--- src/components/PortfolioSlider.js
+++ src/components/PortfolioSlider.js
@@ -3,28 +3,34 @@ import React from "react"
 import Project from "./Project"
 import PortfolioSliderStyles from "./PortfolioSlider.module.css"
 
-// First we get the viewport height and we multiple it by 1% to get a value for a vh unit
-let vh = window.innerHeight * 0.01;
-// Then we set the value in the --vh custom property to the root of the document
-document.documentElement.style.setProperty('--vh', `${vh}px`);
-
-// We listen to the resize event
-window.addEventListener('resize', () => {
-  // We execute the same script as before
-  let vh = window.innerHeight * 0.01;
-  document.documentElement.style.setProperty('--vh', `${vh}px`);
-});
 
 
-const PortfolioSlider = () => (
-  <div className={PortfolioSliderStyles.container}>
-    <span className={PortfolioSliderStyles.spacer}></span>
-    <Project />
-    <Project />
-    <Project />
-    <Project />
-    <span className={PortfolioSliderStyles.spacer}></span>
-  </div>
-)
+export default class PortfolioSlider extends React.Component{
 
-export default PortfolioSlider
+  componentDidMount(){
+    // First we get the viewport height and we multiple it by 1% to get a value for a vh unit
+    let vh = window.innerHeight * 0.01;
+    // Then we set the value in the --vh custom property to the root of the document
+    document.documentElement.style.setProperty('--vh', `${vh}px`);
+
+    // We listen to the resize event
+    window.addEventListener('resize', () => {
+      // We execute the same script as before
+      let vh = window.innerHeight * 0.01;
+      document.documentElement.style.setProperty('--vh', `${vh}px`);
+    });
+  }
+
+  render(){
+    return(
+      <div className={PortfolioSliderStyles.container}>
+        <span className={PortfolioSliderStyles.spacer}></span>
+        <Project />
+        <Project />
+        <Project />
+        <Project />
+        <span className={PortfolioSliderStyles.spacer}></span>
+      </div>
+    )
+  }
+}
