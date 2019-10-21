18d5193e1fff29630cdf01bae152b469b6733111
shrijo <jonas.shriqui@gmail.com>
2019-10-21 09:48:19 +0200

    Imported React100vh

--- src/components/PortfolioSlider.js
+++ src/components/PortfolioSlider.js
@@ -2,16 +2,17 @@ import React from "react"
 
 import Project from "./Project"
 import PortfolioSliderStyles from "./PortfolioSlider.module.css"
+import Div100vh from "../node_modules/react-div-100vh/lib/Div100vh"
 
 const PortfolioSlider = () => (
-  <div className={PortfolioSliderStyles.container}>
+  <Div100vh className={PortfolioSliderStyles.container}>
     <span className={PortfolioSliderStyles.spacer}></span>
     <Project />
     <Project />
     <Project />
     <Project />
     <span className={PortfolioSliderStyles.spacer}></span>
-  </div>
+  </Div100vh>
 )
 
 export default PortfolioSlider
