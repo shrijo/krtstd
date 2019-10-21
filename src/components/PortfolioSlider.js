50e088125700fb275d436137e2923af8c520ff3f
shrijo <jonas.shriqui@gmail.com>
2019-10-21 10:09:57 +0200

    test old vh hack

--- src/components/PortfolioSlider.js
+++ src/components/PortfolioSlider.js
@@ -3,6 +3,19 @@ import React from "react"
 import Project from "./Project"
 import PortfolioSliderStyles from "./PortfolioSlider.module.css"
 
+// First we get the viewport height and we multiple it by 1% to get a value for a vh unit
+let vh = window.innerHeight * 0.01;
+// Then we set the value in the --vh custom property to the root of the document
+document.documentElement.style.setProperty('--vh', `${vh}px`);
+
+// We listen to the resize event
+window.addEventListener('resize', () => {
+  // We execute the same script as before
+  let vh = window.innerHeight * 0.01;
+  document.documentElement.style.setProperty('--vh', `${vh}px`);
+});
+
+
 const PortfolioSlider = () => (
   <div className={PortfolioSliderStyles.container}>
     <span className={PortfolioSliderStyles.spacer}></span>
